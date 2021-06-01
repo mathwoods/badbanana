b9a6e12a46fc7e766bdd0d6bc2db219dfdd10116
Alex Dunne <56739831+dunnesquared@users.noreply.github.com>
2021-06-01 15:40:44 -0400

    Rename driver file

---
+++ play.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+********************************************************************************
+Mathwoods - Bad Banana Redux
+A text-based game in which a player mentally multiplies two whole numbers as
+many times as possible.
+Tested on Python 3.4.1
+Created by Mathwoods, June-July 2017
+Last modified: July 7, 2017
+********************************************************************************
+'''
+from game import game
+
+if __name__ == '__main__':
+    game.run()
